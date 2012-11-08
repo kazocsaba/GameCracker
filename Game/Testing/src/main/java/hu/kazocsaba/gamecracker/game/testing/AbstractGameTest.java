9defbb793a33f7917a1af2a2316b18df5d761200
Kazó Csaba <kazocsaba@gmail.com>
2012-11-08 16:39:20 +0100

    Add facility for testing position serialization.

--- Game/Testing/src/main/java/hu/kazocsaba/gamecracker/game/testing/AbstractGameTest.java
+++ Game/Testing/src/main/java/hu/kazocsaba/gamecracker/game/testing/AbstractGameTest.java
@@ -1,5 +1,10 @@
 package hu.kazocsaba.gamecracker.game.testing;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.ServiceLoader;
@@ -73,5 +78,59 @@ public abstract class AbstractGameTest<G extends Game<P,M,T>, P extends Position
 		assertEquals(game.getCategoryFunction().category(p1), game.getCategoryFunction().category(p2));
 		assertPositionsEqual(p1.transform(t), p2);
 		assertPositionsEqual(p2.transform(t.inverse()), p1);
+	}
+	
+	private static class ByteArrayOutput extends OutputStream {
+		private final byte[] store;
+		private int writtenBytes=0;
+
+		public ByteArrayOutput(byte[] store) {
+			this.store = store;
+		}
+
+		@Override
+		public void write(int b) throws IOException {
+			assertTrue(writtenBytes<store.length, "Too many bytes written");
+			store[writtenBytes]=(byte)b;
+			writtenBytes++;
+		}
+		
+	}
+	private static class ByteArrayInput extends InputStream {
+		private final byte[] store;
+		private int readBytes=0;
+
+		public ByteArrayInput(byte[] store) {
+			this.store = store;
+		}
+
+		@Override
+		public int read() throws IOException {
+			assertTrue(readBytes<store.length, "Too many bytes read");
+			int result=store[readBytes] & 0xFF;
+			readBytes++;
+			return result;
+		}
+		
+	}
+	
+	/**
+	 * Tests the position serializer of the game on the given position. This function checks
+	 * that the number of bytes read is the same as the number of bytes written, and that the
+	 * resulting position is equal to the input (using {@link #assertPositionsEqual(Position, Position)}).
+	 */
+	protected void testPositionSerializer(P position) {
+		byte[] store=new byte[game.getPositionSerializer().getPositionMaxSerializedSize()];
+		try {
+			ByteArrayOutput output=new ByteArrayOutput(store);
+			game.getPositionSerializer().writePosition(position, new DataOutputStream(output));
+			ByteArrayInput input=new ByteArrayInput(store);
+			P readPosition=game.getPositionSerializer().readPosition(new DataInputStream(input));
+			assertEquals(input.readBytes, output.writtenBytes, "Different number of bytes written ("+output.writtenBytes+") and read ("+input.readBytes+")");
+
+			assertPositionsEqual(position, readPosition);
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
 	}
 }
