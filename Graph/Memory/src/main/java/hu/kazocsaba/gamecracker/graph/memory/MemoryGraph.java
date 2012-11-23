6bc503c1a6d726c5d20c58962e7161f622518aca
Kazó Csaba <kazocsaba@gmail.com>
2012-11-23 14:19:57 +0100

    Add some comments to MemoryGraph.expand.

--- Graph/Memory/src/main/java/hu/kazocsaba/gamecracker/graph/memory/MemoryGraph.java
+++ Graph/Memory/src/main/java/hu/kazocsaba/gamecracker/graph/memory/MemoryGraph.java
@@ -48,9 +48,15 @@ public class MemoryGraph<P extends Position<P,M,T>, M extends Move<M,T>, T exten
 		P nextPosition=node.getPosition().move(node.getPosition().getMoves().get(moveIndex));
 		long category=game.getCategoryFunction().category(nextPosition);
 		
+		/*
+		 * We search the category list of the category of the new position, and try to find an equivalent position.
+		 */
 		Node<P,M,T> newNode=null;
 		List<NormalNode<P,M,T>> categoryList=categories.get(category);
 		if (categoryList==null) {
+			/*
+			 * This is a brand new category, there can be no match; we will need a new NormalNode.
+			 */
 			categoryList=new ArrayList<>(4);
 			categories.put(category, categoryList);
 		} else {
@@ -64,18 +70,33 @@ public class MemoryGraph<P extends Position<P,M,T>, M extends Move<M,T>, T exten
 				}
 			}
 			if (match!=null) {
-				if (trans.isIdentity())
+				/*
+				 * We have found that applying trans to match.getPosition produces the new position.
+				 */
+				if (trans.isIdentity()) {
+					/*
+					 * The matching node itself can be used, since it contains the same position.
+					 */
 					newNode=match;
-				else {
-					// try to find a transformation parent of the match with the correct transformation
+				} else {
+					/*
+					 * The transformation is not identity, we need a transformation node with trans. Before we create one,
+					 * let us check if a suitable transformation node parent of our match already exists.
+					 */
 					for (int i=0; i<match.getParentCount(); i++) {
 						Node<P,M,T> parent=match.getParent(i);
-						if (parent instanceof TransformationNode && ((TransformationNode)parent).getTransformation()==trans) {
+						if (parent instanceof TransformationNode && parent.getPosition().equals(nextPosition)) {
+							/*
+							 * We have found the new position in the graph, in a transformation node.
+							 */
 							newNode=parent;
 							break;
 						}
 					}
 					if (newNode==null) {
+						/*
+						 * An exact match for the position is not found, so we add it in a transformation node.
+						 */
 						newNode=new MemoryTransformationNode<>(trans, match, nextPosition);
 						size++;
 					}
@@ -83,11 +104,19 @@ public class MemoryGraph<P extends Position<P,M,T>, M extends Move<M,T>, T exten
 			}
 		}
 		if (newNode==null) {
+			/*
+			 * No match has been found in the graph, so a new normal node is created for the position.
+			 */
 			MemoryNormalNode<P,M,T> newNormalNode=new MemoryNormalNode<>(nextPosition);
 			newNode=newNormalNode;
 			categoryList.add(newNormalNode);
 			size++;
 		}
+		/*
+		 * Hook up the node to its new parent. If its result is known (either because it contains a final position, or
+		 * because it is an existing node found in the graph), this function will trigger a recalculation of the result
+		 * towards the root.
+		 */
 		memnode.setChild(moveIndex, newNode);
 		return newNode;
 	}
